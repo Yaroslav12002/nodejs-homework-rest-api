83d7550b2f6689ff32fcd09f39f9c0748ef28b0c
Yaroslav12002 <my.mail12002@gmail.com>
2023-05-30 15:53:48 +0300

    register+login ready
    Signup and signin ready

---
+++ models/user.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require("mongoose");
+
+const { handleMongooseError } = require("../helpers");
+
+const emailRegexp =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      match: emailRegexp,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      minlength: 6,
+      required: true,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+userSchema.post("save", handleMongooseError);
+
+const User = model("user", userSchema);
+
+module.exports = { User };
